4eec9b7a6f0001fc6d6050ad985b77acff113b1a
Sovithia S <sen.sov@gmail.com>
2021-12-08 15:35:12 +0700

    go
    go

--- api/functions.php
+++ api/functions.php
@@ -2,18 +2,6 @@
 
 require_once 'RestEngine.php';
 
-function getIP()
-{
-	
-	if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-	} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-	    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-	} else {
-	    $ip = $_SERVER['REMOTE_ADDR'];
-	}
-	return $ip
-}
 
 function extractIDS($items,$keyname = "PRODUCTID"){
 	if (count($items) == 0)
