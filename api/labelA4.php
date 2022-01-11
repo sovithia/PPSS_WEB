ab3354f2ab16f36a5a15af97a49e1ad73d9dbd9d
haytongse <haytongse168@gmail.com>
2022-01-11 14:00:23 +0700

    Update Label A4

--- api/labelA4.php
+++ api/labelA4.php
@@ -30,7 +30,7 @@ error_reporting(E_ALL);
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="icon/css/all.min.css">
+<!-- 	<link rel="stylesheet" type="text/css" href="icon/css/all.min.css"> -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
