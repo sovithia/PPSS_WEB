ec7c9597cddd34af7fc049cb82fa2d37417a2def
Sovithia S <sen.sov@gmail.com>
2022-12-17 16:35:33 +0700

    go
    go

--- api/generateScheduledPurchasesBeta.php
+++ api/generateScheduledPurchasesBeta.php
@@ -3,6 +3,130 @@
 require_once("functions.php");
 
 
+function GenerateGroupedPurchasesByDay()
+{
+	$day = strtoupper(date('D'));
+	$indb = getInternalDatabase();
+	$sql = "SELECT ID FROM SUPPLIER WHERE orderday = ?";	
+	$req = $indb->prepare($sql);
+	$vendors = $req->execute(array($day));
+	echo "Total vendors: ".count($vendors)."\n";
+
+	// Check Already generated
+	$vendorGenerated = 0;
+	foreach($vendors as $vendor){
+		$sql = "SELECT * FROM ITEMREQUESTACTION WHERE TYPE = 'GROUPEDPURCHASE' AND ARG1 = ? AND REQUESTEE IS null"; 
+		$req = $indb->prepare($sql);
+		$req->execute(array());
+		$data = $req->execute(PDO::FETCH_ASSOC);
+		if ($data != false)
+			$vendorGenerated++;
+	}
+	echo "Vendor already generated : ".$vendorGenerated."\n";
+
+	foreach($vendors as $vendor){		
+		$sql = "SELECT * FROM ITEMREQUESTACTION WHERE TYPE = 'GROUPEDPURCHASE' AND ARG1 = ? AND REQUESTEE IS null"; 
+		$req = $indb->prepare($sql);
+		$req->execute(array());
+		$data = $req->execute(PDO::FETCH_ASSOC);
+		if ($data != false){
+			echo "VENDOR: ".$vendor["VENDID"]." already generated\n";			
+		}
+		else{
+			// CREATE THE ITEMREQUESTACTION
+			$sql = "SELECT VENDNAME FROM APVENDOR WHERE VENDID = ?";
+			$req = $dbBlue->prepare($sql);
+			$req->execute(array($vendor["VENDID"]));
+			$res = $req->fetch(PDO::FETCH_ASSOC);		
+			if ($res != false)
+				$vendorname = $res["VENDNAME"];
+			else 
+				$vendorname = "";
+			$sql = "SELECT orderday FROM SUPPLIER WHERE ID = ?";
+			$req = $db->prepare($sql);
+			$req->execute(array($vendor["VENDID"]));
+			$res = $req->fetch(PDO::FETCH_ASSOC);
+			if ($res != false)
+				$orderday = $res["orderday"];
+			else 
+				$orderday = false;
+			$db->beginTransaction();
+			$sql = "INSERT INTO ITEMREQUESTACTION (TYPE,REQUESTER,ARG1,ARG2,ARG3) VALUES ('GROUPEDPURCHASE','AUTO',?,?,?)";
+			$req = $db->prepare($sql);
+			$req->execute(array($vendor["VENDID"],$vendorname,$orderday));
+			$theID = $db->lastInsertId();
+			$db->commit();
+		}
+		// SELECT ALL ITEM EXCEPT BLACKLIST
+		$sql = "SELECT ICPRODUCT.PRODUCTID,ONHAND,PRODUCTNAME 
+		FROM ICPRODUCT
+		WHERE ACTIVE = 1					
+		AND PPSS_IS_BLACKLIST IS NULL		
+		AND PPSS_HAVE_EXTERNAL IS NULL
+		AND PPSS_IS_ORDERED IS NULL
+		AND VENDID = ?";		
+		$req = $dbBlue->prepare($sql);
+		$req->execute(array($vendor["VENDID"]));
+		$items = $req->fetchAll(PDO::FETCH_ASSOC);
+		echo "Nb items: ".count($items)."\n"; 
+
+		foreach($items as $item){	
+			$sql = "SELECT * FROM ITEMREQUEST WHERE PRODUCTID = ? AND ITEMREQUESTACTION_ID = ?";
+			$req = $db->prepare($sql);
+			$req->execute(array($item["PRODUCTID"],$theID));
+			$res = $req->fetch(PDO::FETCH_ASSOC);
+			if ($res != false){
+				echo $item["PRODUCTID"] . " already generated\n";
+				continue;
+			}					
+			$sql = "SELECT TOP(1) TRANCOST,TRANDISC,TRANQTY FROM PORECEIVEDETAIL WHERE PRODUCTID = ? ORDER BY COLID DESC";			
+			$req = $dbBlue->prepare($sql);
+			$req->execute(array($item["PRODUCTID"]));
+			$res = $req->fetch(PDO::FETCH_ASSOC);
+			$sql = "SELECT PPSS_NEW_COST FROM ICPRODUCT WHERE PRODUCTID = ?";
+			$req = $dbBlue->prepare($sql);
+			$req->execute(array($item["PRODUCTID"]));
+			$res2 = $req->fetch(PDO::FETCH_ASSOC);
+			if ($res2 == false || $res2["PPSS_NEW_COST"] == null){
+				if ($res == false)
+					$TRANCOST = 0;
+				else
+					$TRANCOST = $res["TRANCOST"];
+			}				
+			else{
+				if ($res2["TRANCOST"] != "0" || $res2["TRANCOST"] != 0)
+					$TRANCOST = $res2["PPSS_NEW_COST"];
+				else 
+					$TRANCOST = $res["TRANCOST"];
+			}
+								
+			if ($new == true){
+				$stats = orderStatistics($item["PRODUCTID"]);						
+				echo $item["PRODUCTID"]."-".$item["PRODUCTNAME"].":".$stats["DECISION"]." ORDERPOINT:".$stats["ORDERPOINT"]. " QTY:".$stats["FINALQTY"]." ONHAND: ".$item["ONHAND"]."\n";
+				// SHOULD WE EXCLUDE TOO EARLY ITEMS ? 				
+				$sql = "INSERT INTO ITEMREQUEST (
+				PRODUCTID,REQUEST_QUANTITY,COST,DISCOUNT,LASTRECEIVEDATE,
+				LASTRECEIVEQUANTITY,MOMENTONHAND,TOTALWASTEQUANTITY,TOTALPROMOTIONQUANTITY,SALESINCELASTRECEIVE,
+				SALE70PERCENTDAYS,LASTSALEDAY,TOTALORDERTIME,TOTALRECEIVE,TOTALSALE,
+				CALCULATED_QUANTITY,DECISION,REQUESTTYPE,ITEMREQUESTACTION_ID) 
+				VALUES (?,?,?,?,?,
+						?,?,?,?,?,
+						?,?,?,?,?,
+						?,?,?,?)";
+				$req = $db->prepare($sql);
+				$req->execute(array(
+				$item["PRODUCTID"],$stats["FINALQTY"], $TRANCOST,$res["TRANDISC"],$stats["LASTRCVDATE"],
+				$stats["LASTRECEIVEQUANTITY"],$stats["ONHAND"],$stats["WASTE"],$stats["PROMO"],$stats["SALESINCELASTRECEIVE"],
+				$stats["SALESPEED"],$stats["LASTSALEDAY"],$stats["TOTALORDERTIME"],$stats["TOTALRECEIVE"],$stats["TOTALSALE"],
+				$stats["FINALQTY"],$stats["DECISION"],'ALGO',$theID));										
+			}				
+			$sql = "UPDATE ICPRODUCT SET PPSS_IS_ORDERED = 'Y' WHERE PRODUCTID = ?";
+			$req = $dbBlue->prepare($sql);
+			$req->execute(array($item["PRODUCTID"]));	
+			sleep(1);	
+		}	
+	}		
+}
 
 function GenerateDailyGroupedPurchases()
 {
@@ -168,6 +292,6 @@ function GenerateDailyGroupedPurchases()
 	}
 }
 
-//GenerateDailyGroupedPurchases();
+GenerateDailyGroupedPurchases();
 
 ?>
